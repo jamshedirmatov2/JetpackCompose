aeb4243ca294c5df9598f60781fe5f047ebab5e4
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-12 20:48:44 +0500

    Instagram finished

--- app/src/main/java/com/wigroup/jetpackcompose/ui/component/InstagramProfileCard.kt
+++ app/src/main/java/com/wigroup/jetpackcompose/ui/component/InstagramProfileCard.kt
@@ -4,14 +4,14 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -19,14 +19,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.wigroup.jetpackcompose.R
-import com.wigroup.jetpackcompose.ui.theme.JetpackComposeTheme
 
 @Composable
 fun InstagramProfileCard() {
@@ -36,21 +36,44 @@ fun InstagramProfileCard() {
         shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
+            modifier = Modifier.padding(16.dp),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_instagram),
-                contentDescription = "Logo Profile",
-                modifier = Modifier.size(50.dp),
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_instagram),
+                    contentDescription = "Logo Profile",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(8.dp),
+                )
+                UserStatic(title = "Posts", value = "6,950")
+                UserStatic(title = "Followers", value = "436M")
+                UserStatic(title = "Following", value = "76")
+            }
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive,
             )
-            UserStatic(title = "Posts", value = "6,950")
-            UserStatic(title = "Followers", value = "436M")
-            UserStatic(title = "Following", value = "76")
+            Text(
+                text = "#YoursToMake",
+                fontSize = 14.sp,
+            )
+            Text(
+                text = "www.facebook.com/emotional_health",
+                fontSize = 14.sp,
+            )
+            Button(onClick = {}) {
+                Text(text = "Follow")
+            }
         }
     }
 }
@@ -73,33 +96,5 @@ private fun UserStatic(
             text = title,
             fontWeight = FontWeight.Bold,
         )
-    }
-}
-
-@Preview
-@Composable
-private fun DarkPreview() {
-    JetpackComposeTheme(true) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            InstagramProfileCard()
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun LightPreview() {
-    JetpackComposeTheme(false) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            InstagramProfileCard()
-        }
     }
 }
