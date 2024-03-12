f3f632fd8beb9523c3cd793e5b93597606246aec
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-12 19:35:44 +0500

    Text

--- app/src/main/java/com/wigroup/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/wigroup/jetpackcompose/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.wigroup.jetpackcompose.ui.component.InstagramProfileCard
 import com.wigroup.jetpackcompose.ui.theme.JetpackComposeTheme
 
@@ -26,34 +24,6 @@ class MainActivity : ComponentActivity() {
                     InstagramProfileCard()
                 }
             }
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun DarkPreview() {
-    JetpackComposeTheme(true) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            InstagramProfileCard()
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun LightPreview() {
-    JetpackComposeTheme(false) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            InstagramProfileCard()
         }
     }
 }
--- app/src/main/java/com/wigroup/jetpackcompose/ui/component/InstagramProfileCard.kt
+++ app/src/main/java/com/wigroup/jetpackcompose/ui/component/InstagramProfileCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,11 +14,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wigroup.jetpackcompose.ui.theme.JetpackComposeTheme
 
 @Composable
 fun InstagramProfileCard() {
@@ -39,28 +46,58 @@ fun InstagramProfileCard() {
                     .size(50.dp)
                     .background(color = Color.Yellow)
             )
-            TwoBoxes()
-            TwoBoxes()
-            TwoBoxes()
+            UserStatic(title = "Posts", value = "6,950")
+            UserStatic(title = "Followers", value = "436M")
+            UserStatic(title = "Following", value = "76")
         }
     }
 }
 
 @Composable
-private fun TwoBoxes() {
+private fun UserStatic(
+    title: String,
+    value: String,
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
     ) {
+        Text(
+            text = value,
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Cursive,
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DarkPreview() {
+    JetpackComposeTheme(true) {
         Box(
             modifier = Modifier
-                .size(25.dp)
-                .background(color = Color.Blue)
-        )
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            InstagramProfileCard()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LightPreview() {
+    JetpackComposeTheme(false) {
         Box(
             modifier = Modifier
-                .size(25.dp)
-                .background(color = Color.Red)
-        )
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            InstagramProfileCard()
+        }
     }
 }
