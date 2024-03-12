ad5c468f5cf94628ecdeb8a1482edaf118bbfbc6
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-12 21:30:19 +0500

    Instagram finished

--- app/src/main/java/com/wigroup/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/wigroup/jetpackcompose/MainActivity.kt
@@ -3,10 +3,9 @@ package com.wigroup.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.wigroup.jetpackcompose.ui.component.InstagramProfileCard
 import com.wigroup.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -16,10 +15,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     InstagramProfileCard()
                 }
