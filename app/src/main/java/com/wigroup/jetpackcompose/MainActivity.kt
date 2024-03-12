83fa3bc75f5b7846bec504ee3e0a758395d1b793
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-12 17:13:11 +0500

    Card

--- app/src/main/java/com/wigroup/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/wigroup/jetpackcompose/MainActivity.kt
@@ -3,13 +3,13 @@ package com.wigroup.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.wigroup.jetpackcompose.ui.component.TimesTable
+import com.wigroup.jetpackcompose.ui.component.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
+            InstagramProfileCard()
         }
     }
 }
--- app/src/main/java/com/wigroup/jetpackcompose/ui/component/InstagramProfileCard.kt
+++ app/src/main/java/com/wigroup/jetpackcompose/ui/component/InstagramProfileCard.kt
@@ -1,5 +1,6 @@
 package com.wigroup.jetpackcompose.ui.component
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,6 +9,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,21 +22,27 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically,
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        border = BorderStroke(1.dp, Color.Red),
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(color = Color.Yellow)
-        )
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Yellow)
+            )
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+        }
     }
 }
 
