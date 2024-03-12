748823db4bdacbdf93db0ccd2cc8782404c8d45d
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-12 16:30:12 +0500

    Instagram Profile Card part 1

---
+++ app/src/main/java/com/wigroup/jetpackcompose/ui/component/TimesTable.kt
@@ -0,0 +1,47 @@
+package com.wigroup.jetpackcompose.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun TimesTable() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val color = if ((i + j) % 2 == 0) Color.Yellow else Color.White
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .background(color = color)
+                            .border(width = 1.dp, color = Color.Black),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${i * j}")
+                    }
+                }
+            }
+        }
+    }
+}
